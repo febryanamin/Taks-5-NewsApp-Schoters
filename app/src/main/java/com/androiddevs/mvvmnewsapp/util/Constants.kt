fc3aeece5ebb99faaf9531e37e5acf57c6eb5db4
febryanamin <ahmad.f.amin.967@gmail.com>
2023-05-04 22:33:11 +0700

    Retrofit Setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "899223bea6cc41508268a1f219ba3efa"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
