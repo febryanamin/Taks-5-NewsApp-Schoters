01e0b8afc4d3dcf406e78e7e86a76eaa2548d036
febryanamin <ahmad.f.amin.967@gmail.com>
2023-05-12 00:35:54 +0700

    Searching News

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val COUNTRY_CODE = "us"
         const val API_KEY = "899223bea6cc41508268a1f219ba3efa"
         const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
