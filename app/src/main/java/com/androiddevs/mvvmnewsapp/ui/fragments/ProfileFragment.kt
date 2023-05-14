4d7bc47cfceb980b2d791b65f67ff9b8b97a2e5f
febryanamin <ahmad.f.amin.967@gmail.com>
2023-05-15 06:08:50 +0700

    setting profile

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+}
